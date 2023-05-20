1f3698e907527f0444d84f58baa5a622f3d25430
Cozonac Ion <fresh.go.ionutz@gmail.com>
2023-05-20 18:12:16 +0300

    .

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // Funcții JavaScript
+
 function calculateTripDuration(startDate, endDate) {
   const start = new Date(startDate);
   const end = new Date(endDate);
@@ -13,8 +14,29 @@ function formatDate(date) {
 }
 
 function saveTripToLocalStorage(trip) {
-  const trips = JSON.parse(localStorage.getItem("trips"));
+  let trips = JSON.parse(localStorage.getItem("trips"));
+  if (!trips) {
+    trips = [];
+  }
+
   trip.id = Date.now();
+
+  if (
+    trip.transport &&
+    trip.transport.transportType &&
+    trip.transport.transportDetails
+  ) {
+    // Nu face nimic - transportul este deja definit corect
+  } else {
+    trip.transport = null;
+  }
+
+  if (trip.info && trip.info.otherInfo) {
+    // Nu face nimic - info este deja definit corect
+  } else {
+    trip.info = null;
+  }
+
   trips.push(trip);
   localStorage.setItem("trips", JSON.stringify(trips));
 }
@@ -49,7 +71,14 @@ function validateForm(destination, startDate, endDate) {
 
 // Obiecte JavaScript
 const Trip = {
-  create(destination, startDate, endDate) {
+  create(
+    destination,
+    startDate,
+    endDate,
+    transportType,
+    transportDetails,
+    otherInfo
+  ) {
     const duration = calculateTripDuration(startDate, endDate);
     const formattedStartDate = formatDate(startDate);
     const formattedEndDate = formatDate(endDate);
@@ -59,6 +88,27 @@ const Trip = {
       startDate: formattedStartDate,
       endDate: formattedEndDate,
       duration,
+      transport: transportType
+        ? Transport.create(transportType, transportDetails)
+        : null,
+      info: otherInfo ? Info.create(otherInfo) : null,
+    };
+  },
+};
+
+const Transport = {
+  create(transportType, transportDetails) {
+    return {
+      transportType,
+      transportDetails,
+    };
+  },
+};
+
+const Info = {
+  create(otherInfo) {
+    return {
+      otherInfo,
     };
   },
 };
@@ -68,14 +118,30 @@ function addTripToDOM(trip) {
   const tripList = document.getElementById("travel-list");
   const tripItem = document.createElement("div");
   tripItem.classList.add("trip-item");
-  tripItem.innerHTML = `
-      <h3>${trip.destination}</h3>
-      <p>Start: ${trip.startDate}</p>
-      <p>End: ${trip.endDate}</p>
-      <p>Duration: ${trip.duration} days</p>
-      <button class="delete-button" data-id="${trip.id}">X</button>
-      <button class="info-button" data-id="${trip.id}">Info</button>
-    `;
+
+  let tripDetails = `
+    <h3>${trip.destination}</h3>
+    <p>Start: ${trip.startDate}</p>
+    <p>End: ${trip.endDate}</p>
+    <p>Duration: ${trip.duration} days</p>
+  `;
+
+  if (
+    trip.transport &&
+    trip.transport.transportType &&
+    trip.transport.transportDetails
+  ) {
+    tripDetails += `<p>Transport: ${trip.transport.transportType}</p>`;
+    tripDetails += `<p>Transport Details: ${trip.transport.transportDetails}</p>`;
+  }
+
+  if (trip.info && trip.info.otherInfo) {
+    tripDetails += `<p>Other Info: ${trip.info.otherInfo}</p>`;
+  }
+
+  tripDetails += `<button class="delete-button" data-id="${trip.id}">X</button>`;
+
+  tripItem.innerHTML = tripDetails;
   tripList.appendChild(tripItem);
 }
 
@@ -114,10 +180,18 @@ document.addEventListener("DOMContentLoaded", function () {
     const destinationInput = document.getElementById("destination-input");
     const startDateInput = document.getElementById("start-date-input");
     const endDateInput = document.getElementById("end-date-input");
+    const transportTypeInput = document.getElementById("transport-input");
+    const transportDetailsInput = document.getElementById(
+      "transport-details-input"
+    );
+    const otherInfoInput = document.getElementById("other-info-input");
 
     const destination = destinationInput.value;
     const startDate = startDateInput.value;
     const endDate = endDateInput.value;
+    const transportType = transportTypeInput.value;
+    const transportDetails = transportDetailsInput.value;
+    const otherInfo = otherInfoInput.value;
 
     if (!validateForm(destination, startDate, endDate)) {
       alert("Vă rugăm să completați toate câmpurile corect.");
@@ -125,12 +199,21 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     const trip = Trip.create(destination, startDate, endDate);
+    const transport = Transport.create(transportType, transportDetails);
+    const info = Info.create(otherInfo);
+
+    trip.transport = transport;
+    trip.info = info;
+
     saveTripToLocalStorage(trip);
     addTripToDOM(trip);
 
     destinationInput.value = "";
     startDateInput.value = "";
     endDateInput.value = "";
+    transportTypeInput.value = "null";
+    transportDetailsInput.value = "";
+    otherInfoInput.value = "";
   });
 
   // Încărcare călătorii salvate din localStorage la încărcarea paginii
